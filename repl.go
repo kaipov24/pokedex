05dc12831a54b13df5fad3252fe0b64ba7730575
Kayrat Kaipov <kaipovich24@gmail.com>
2025-03-22 22:25:56 +0600

    Registry

--- repl.go
+++ repl.go
@@ -7,20 +7,63 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Displays a help message")
+	return nil
+}
+
+func keyContains(m map[string]cliCommand, substr string) bool {
+	for key := range m {
+		if strings.Contains(key, substr) {
+			return true
+		}
+	}
+	return false
+}
+
 func startRepl() {
+
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	mapCliCommands := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Show this help message",
+			callback:    commandHelp,
+		},
+	}
+
 	reader := bufio.NewScanner(os.Stdin)
+
 	for {
 		fmt.Print("Pokedex > ")
 		reader.Scan()
 
-		words := cleanInput(reader.Text())
-		if len(words) == 0 {
-			continue
+		command := reader.Text()
+		if keyContains(mapCliCommands, command) {
+			mapCliCommands[command].callback()
+
+		} else {
+			fmt.Println("Unknown command")
 		}
 
-		commandName := words[0]
-
-		fmt.Printf("Your command was: %s\n", commandName)
 	}
 }
 
