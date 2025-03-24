eeba6810363f63ea7ec1475f2fa5479198601136
Kayrat Kaipov <kaipovich24@gmail.com>
2025-03-24 23:29:51 +0600

    Map first

--- command_map.go
+++ command_map.go
@@ -1,9 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type LocationResponse struct {
+	Results []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
 
 func commandMap() error {
+	const baseURL = "https://pokeapi.co/api/v2/location-area/"
+	resp, err := http.Get(baseURL)
+	if err != nil {
+		return fmt.Errorf("failed to get location areas: %w", err)
+	}
+	defer resp.Body.Close()
 
-	fmt.Println("Displaying the names of 20 location areas")
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error reading response:", err)
+		return nil
+	}
+
+	var locations LocationResponse
+	err = json.Unmarshal(body, &locations)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, location := range locations.Results {
+		fmt.Println(location.Name)
+	}
+
 	return nil
 }
